0b28a8a5ca2a2cc1789389c4e62669d98c173d7d
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-16 22:53:02 +0300

    Added getting data from edits

--- app/src/main/java/com/mikhailovskii/weatherandroid/MainActivity.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mikhailovskii.weatherandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sign_in_btn.setOnClickListener {
+            println(login_et.text.toString() + " " + password_et.text.toString())
+        }
     }
 }
