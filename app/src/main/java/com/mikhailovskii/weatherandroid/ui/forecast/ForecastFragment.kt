cf88d2f1d47a66c93d4e2857314d2f2cc9187fd5
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-20 14:24:55 +0300

    Added Forecast fragment

---
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/forecast/ForecastFragment.kt
@@ -0,0 +1,23 @@
+package com.mikhailovskii.weatherandroid.ui.forecast
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mikhailovskii.weatherandroid.R
+
+class ForecastFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecast, container, false)
+    }
+
+
+}
