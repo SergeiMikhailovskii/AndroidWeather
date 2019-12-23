50a29e78a39dfcbdceb1b36a17b1c04c31ecf6f6
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-23 11:48:17 +0300

    Added permissions for getting location

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsFragment.kt
@@ -1,10 +1,13 @@
 package com.mikhailovskii.weatherandroid.ui.maps
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -31,8 +34,34 @@ class MapsFragment : Fragment() {
             run {
                 this.googleMap = googleMap!!
 
+                if (ActivityCompat.checkSelfPermission(
+                        context!!,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(
+                        context!!,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions(
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ), 100
+                    )
+                }
+
+                googleMap.isIndoorEnabled = true
+
+                val uiSettings = googleMap.uiSettings
+                uiSettings.isIndoorLevelPickerEnabled = true
+                uiSettings.isMapToolbarEnabled = true
+                uiSettings.isCompassEnabled = true
+                uiSettings.isZoomControlsEnabled = true
+
                 val latLng = LatLng(40.0, (-74).toDouble())
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                googleMap.isMyLocationEnabled = true
                 map_view.onResume()
             }
         }
