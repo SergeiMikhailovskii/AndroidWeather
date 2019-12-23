c2f070a8848200deee7302680961e9facf90b2fa
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-23 11:06:45 +0300

    Fixed showing of MapView

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsFragment.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 import com.mikhailovskii.weatherandroid.R
 import kotlinx.android.synthetic.main.fragment_maps.*
 
 class MapsFragment : Fragment() {
 
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -23,8 +27,14 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         map_view.onCreate(savedInstanceState)
-        map_view.getMapAsync {
-        }
+        map_view.getMapAsync { googleMap ->
+            run {
+                this.googleMap = googleMap!!
 
+                val latLng = LatLng(40.0, (-74).toDouble())
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                map_view.onResume()
+            }
+        }
     }
 }
