69ab093b23df2bc21a8ad51d2de4686e0a93bc77
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-24 17:01:51 +0300

    Added saving data to Firebase Realtime Database

--- app/src/main/java/com/mikhailovskii/weatherandroid/data/entities/User.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/data/entities/User.kt
@@ -2,10 +2,10 @@ package com.mikhailovskii.weatherandroid.data.entities
 
 data class User(
 
-    var login: String?,
-    var password: String?,
-    var twitterKey: String?,
-    var facebookKey: String?,
-    var googleKey: String?
+    var login: String? = "",
+    var password: String? = "",
+    var twitterKey: String? = "",
+    var facebookKey: String? = "",
+    var googleKey: String? = ""
 
 )
--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
@@ -1,8 +1,8 @@
 package com.mikhailovskii.weatherandroid.ui.login
 
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.mikhailovskii.weatherandroid.data.entities.User
 import com.mikhailovskii.weatherandroid.ui.base.BasePresenter
 
 class LoginPresenter : BasePresenter<LoginContract.LoginView>(), LoginContract.LoginPresenter {
@@ -11,7 +11,16 @@ class LoginPresenter : BasePresenter<LoginContract.LoginView>(), LoginContract.L
 
     override fun saveUserData(bundle: Bundle) {
         database = FirebaseDatabase.getInstance().reference
+
+        val login = bundle.getString("login")
+        val password = bundle.getString("password")
+
+        val user = User(login = login, password = password)
+
+        database.child("users").push().setValue(user)
+
         view?.onLoggedIn()
+
     }
 
 }
