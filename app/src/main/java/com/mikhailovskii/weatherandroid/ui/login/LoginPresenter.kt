9bf5d2bb5c09338b8c89fe2657027bd942ffb796
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-24 16:09:40 +0300

    Added User entity

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
@@ -1,11 +1,16 @@
 package com.mikhailovskii.weatherandroid.ui.login
 
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.mikhailovskii.weatherandroid.ui.base.BasePresenter
 
 class LoginPresenter : BasePresenter<LoginContract.LoginView>(), LoginContract.LoginPresenter {
 
+    private lateinit var database: DatabaseReference
+
     override fun saveUserData(bundle: Bundle) {
+        database = FirebaseDatabase.getInstance().reference
         view?.onLoggedIn()
     }
 
