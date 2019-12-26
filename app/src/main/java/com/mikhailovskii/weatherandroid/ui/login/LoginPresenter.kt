7ff94b2aebf4b11b7b1e7b88025d8faa133bcdaf
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-26 18:09:56 +0300

    Added getting user info while authorization

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/login/LoginPresenter.kt
@@ -1,16 +1,17 @@
 package com.mikhailovskii.weatherandroid.ui.login
 
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.mikhailovskii.weatherandroid.AndroidWeatherApp
 import com.mikhailovskii.weatherandroid.data.entities.User
 import com.mikhailovskii.weatherandroid.ui.base.BasePresenter
 import com.mikhailovskii.weatherandroid.util.Preference
+import timber.log.Timber
 
 class LoginPresenter : BasePresenter<LoginContract.LoginView>(), LoginContract.LoginPresenter {
 
     private lateinit var database: DatabaseReference
+    private lateinit var query: Query
 
     override fun saveUserData(bundle: Bundle) {
         database = FirebaseDatabase.getInstance().reference
@@ -20,7 +21,32 @@ class LoginPresenter : BasePresenter<LoginContract.LoginView>(), LoginContract.L
 
         val user = User(login = login, password = password)
 
-        database.child("users").push().setValue(user)
+        query = database.child("users").orderByChild("login").equalTo(login)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onCancelled(error: DatabaseError) {
+                Timber.e(error.toException())
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    val users = snapshot.children
+
+                    users.forEach { data ->
+                        val userData = data.getValue(User::class.java)
+                        Timber.i("LOGIN ${userData?.login}")
+                    }
+
+                } else {
+                    Timber.e("NO SUCH USER $login $password")
+
+                    database.child("users").push().setValue(user)
+                }
+
+            }
+
+        })
 
         Preference.getInstance(AndroidWeatherApp.appContext).user = user
 
