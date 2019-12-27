f4603266fd2e5d71d60c3a0377bb9070c01c5a67
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-27 15:12:20 +0300

    Added CompositeDisposable

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/base/BasePresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/base/BasePresenter.kt
@@ -1,14 +1,18 @@
 package com.mikhailovskii.weatherandroid.ui.base
 
+import io.reactivex.disposables.CompositeDisposable
+
 open class BasePresenter<View> : MvpPresenter<View> {
 
     var view: View? = null
+    var compositeDisposable = CompositeDisposable()
 
     override fun attachView(view: View) {
         this.view = view
     }
 
     override fun detachView() {
+        compositeDisposable.clear()
         this.view = null
     }
 }
