c354cd400eb3fe39e19b236d1caa0e7f94123693
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-03 12:48:34 +0300

    Removed MapsAPI classes

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsPresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/maps/MapsPresenter.kt
@@ -1,13 +1,8 @@
 package com.mikhailovskii.weatherandroid.ui.maps
 
 import com.mikhailovskii.weatherandroid.AndroidWeatherApp
-import com.mikhailovskii.weatherandroid.data.api.maps.MapsAPIFactory
 import com.mikhailovskii.weatherandroid.ui.base.BasePresenter
 import com.mikhailovskii.weatherandroid.util.Preference
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MapsPresenter : BasePresenter<MapsContract.MapsView>(), MapsContract.MapsPresenter {
 
