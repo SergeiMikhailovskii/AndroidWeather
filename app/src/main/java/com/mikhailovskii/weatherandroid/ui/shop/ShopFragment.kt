9210f91afdad71310b7c325fc3875b9b06028c4a
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-08 12:29:24 +0300

    Added gradient background and navigation from bottom navigation

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/shop/ShopFragment.kt
@@ -1,12 +1,14 @@
 package com.mikhailovskii.weatherandroid.ui.shop
 
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mikhailovskii.weatherandroid.R
+import kotlinx.android.synthetic.main.fragment_shop.*
 
 class ShopFragment : Fragment(), ShopContract.ShopView {
 
@@ -17,6 +19,17 @@ class ShopFragment : Fragment(), ShopContract.ShopView {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_shop, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gradientDrawable = GradientDrawable(
+            GradientDrawable.Orientation.TL_BR,
+            intArrayOf(0xff3f5fa4.toInt(), 0xff17253e.toInt())
+        )
+
+        stickers_list.background = gradientDrawable
     }
 
     override fun showEmptyState(value: Boolean) {
