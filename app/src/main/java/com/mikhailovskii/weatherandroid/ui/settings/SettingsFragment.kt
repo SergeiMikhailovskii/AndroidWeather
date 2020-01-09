364311acd5b9b17e2fba8d0e039f8190bab7a690
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-09 10:49:15 +0300

    Added Gradient background

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/settings/SettingsFragment.kt
@@ -1,12 +1,14 @@
 package com.mikhailovskii.weatherandroid.ui.settings
 
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mikhailovskii.weatherandroid.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment(), SettingsContract.SettingsView {
 
@@ -16,6 +18,16 @@ class SettingsFragment : Fragment(), SettingsContract.SettingsView {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val gradientDrawable = GradientDrawable(
+            GradientDrawable.Orientation.TL_BR,
+            intArrayOf(0xffa35043.toInt(), 0xffcd7d5c.toInt())
+        )
+
+        scrollView.background = gradientDrawable
     }
 
     override fun showEmptyState(value: Boolean) {
