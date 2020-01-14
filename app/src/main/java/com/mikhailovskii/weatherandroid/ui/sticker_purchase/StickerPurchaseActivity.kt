188ca922eab99aba0d05df9e7941a65d1ebb7922
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-14 16:52:01 +0300

    Added passing sticker pack to adapter and changed retrofit to object

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/sticker_purchase/StickerPurchaseActivity.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/sticker_purchase/StickerPurchaseActivity.kt
@@ -1,14 +1,19 @@
 package com.mikhailovskii.weatherandroid.ui.sticker_purchase
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.mikhailovskii.weatherandroid.R
 import com.mikhailovskii.weatherandroid.data.entities.StickerPack
-import com.mikhailovskii.weatherandroid.util.showSuccessToast
+import com.mikhailovskii.weatherandroid.ui.adapter.StickerPackAdapter
+import kotlinx.android.synthetic.main.activity_sticker_purchase.*
 
 class StickerPurchaseActivity : AppCompatActivity(), StickerPurchaseContract.StickerPurchaseView {
 
     private val presenter = StickerPurchasePresenter()
+    private var adapter: StickerPackAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +21,28 @@ class StickerPurchaseActivity : AppCompatActivity(), StickerPurchaseContract.Sti
 
         presenter.attachView(this)
 
+        stickers_grid.layoutManager = GridLayoutManager(applicationContext, 2)
+        adapter = StickerPackAdapter()
+        stickers_grid.adapter = adapter
+
         val name = intent.getStringExtra(EXTRA_NAME)
 
         presenter.getStickerPackByName(name!!)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onStickerPackByNameLoaded(stickerPack: StickerPack) {
+        val stickersList = ArrayList<String>()
 
+        for (sticker in stickerPack.stickers!!) {
+            stickersList.add(sticker)
+        }
+
+        adapter?.setData(stickersList)
+        title_tv.text = stickerPack.title
+        buy_btn.text = "${stickerPack.price} $"
+
+        Glide.with(applicationContext).load(stickersList[0]).into(sticker_iv)
     }
 
     override fun onStickerPackByNameFailed() {
