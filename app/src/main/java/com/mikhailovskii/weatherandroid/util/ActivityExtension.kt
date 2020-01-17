86c287f2823320cbe13b1949bf87eec3885f714b
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-17 15:01:14 +0300

    Created ActivityExtension and FragmnentExtension

---
+++ app/src/main/java/com/mikhailovskii/weatherandroid/util/ActivityExtension.kt
@@ -0,0 +1,28 @@
+package com.mikhailovskii.weatherandroid.util
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+fun AppCompatActivity.replaceFragment(
+    fragment: Fragment,
+    frameId: Int,
+    addBackToStack: Boolean = false
+) {
+    var transactionFragment = fragment
+    val existingFragment = supportFragmentManager.findFragmentByTag(fragment.tag)
+    val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+    existingFragment?.let {
+        transactionFragment = existingFragment
+    }
+
+    if (!supportFragmentManager.isStateSaved) {
+        if (addBackToStack) {
+            fragmentTransaction.addToBackStack(transactionFragment.javaClass.simpleName)
+        }
+
+        fragmentTransaction
+            .replace(frameId, transactionFragment, transactionFragment.name())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/mikhailovskii/weatherandroid/util/FragmentExtension.kt
@@ -0,0 +1,5 @@
+package com.mikhailovskii.weatherandroid.util
+
+import androidx.fragment.app.Fragment
+
+fun Fragment.name(): String = this.javaClass.simpleName
