3f149ea6d81d022910611fe8de677de8eb7183e3
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-22 12:24:46 +0300

    Moved getWindDirection to WeatherUtils

--- app/src/main/java/com/mikhailovskii/weatherandroid/util/WeatherCalculations.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/util/WeatherCalculations.kt
@@ -4,20 +4,6 @@ import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun getWindDirection(degree: Int): String {
-    return when {
-        degree > 337.5 -> "N"
-        degree > 292.5 -> "NW"
-        degree > 247.5 -> "W"
-        degree > 202.5 -> "SW"
-        degree > 157.5 -> "S"
-        degree > 122.5 -> "SE"
-        degree > 67.5 -> "E"
-        degree > 22.5 -> "NE"
-        else -> "N"
-    }
-}
-
 @SuppressLint("SimpleDateFormat")
 fun getDateFromSeconds(seconds: Long): String {
     val simpleDateFormat = SimpleDateFormat("E HH:mm")
@@ -32,5 +18,20 @@ class WeatherUtils {
         private const val KELVIN_TO_CELSIUS_COEFFICIENT = 273
 
         fun convertKelvinToCelsius(kelvin: Double): Double = kelvin - KELVIN_TO_CELSIUS_COEFFICIENT
+
+        fun getWindDirection(degree: Int): String {
+            return when {
+                degree > 337.5 -> "N"
+                degree > 292.5 -> "NW"
+                degree > 247.5 -> "W"
+                degree > 202.5 -> "SW"
+                degree > 157.5 -> "S"
+                degree > 122.5 -> "SE"
+                degree > 67.5 -> "E"
+                degree > 22.5 -> "NE"
+                else -> "N"
+            }
+        }
+
     }
 }
