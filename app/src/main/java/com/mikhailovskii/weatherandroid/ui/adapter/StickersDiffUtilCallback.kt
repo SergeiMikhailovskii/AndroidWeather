19c1e2ecce9300ef0625f8056ac7c853a265e84d
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-22 10:56:45 +0300

    Refactored StickersDiffUtilCallback

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/adapter/StickersDiffUtilCallback.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/adapter/StickersDiffUtilCallback.kt
@@ -8,13 +8,13 @@ class StickersDiffUtilCallback : DiffUtil.ItemCallback<StickerPack>() {
     override fun areItemsTheSame(oldItem: StickerPack, newItem: StickerPack): Boolean = true
 
     override fun areContentsTheSame(oldItem: StickerPack, newItem: StickerPack): Boolean =
-        if (oldItem.stickers?.isNotEmpty() == true) {
+        if (oldItem.stickers?.isNotEmpty() == true && newItem.stickers?.isNotEmpty() == true) {
             (oldItem.title == newItem.title
                     && oldItem.stickers?.get(0) == newItem.stickers?.get(0)
                     && oldItem.price == newItem.price)
-        } else {
+        } else if (oldItem.stickers?.isEmpty() == true && newItem.stickers?.isEmpty() == true) {
             (oldItem.title == newItem.title
                     && oldItem.price == newItem.price)
-        }
+        } else false
 
 }
