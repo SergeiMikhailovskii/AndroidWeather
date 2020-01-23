bd451ceca026454e94ad88cd1e3a43d9e5267b7a
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-23 10:44:14 +0300

    Added ChooseLocationActivity

---
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/choose_location/ChooseLocationActivity.kt
@@ -0,0 +1,14 @@
+package com.mikhailovskii.weatherandroid.ui.choose_location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mikhailovskii.weatherandroid.R
+
+class ChooseLocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_location)
+    }
+
+}
