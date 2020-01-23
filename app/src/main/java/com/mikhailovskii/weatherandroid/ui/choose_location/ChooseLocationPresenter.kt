a47057810928e2ff48dc3cf66c391a2bc3876462
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-23 12:11:03 +0300

    Implemented saving to preferences in presenter

--- app/src/main/java/com/mikhailovskii/weatherandroid/ui/choose_location/ChooseLocationPresenter.kt
+++ app/src/main/java/com/mikhailovskii/weatherandroid/ui/choose_location/ChooseLocationPresenter.kt
@@ -1,12 +1,17 @@
 package com.mikhailovskii.weatherandroid.ui.choose_location
 
 import com.mikhailovskii.weatherandroid.ui.base.BasePresenter
+import com.mikhailovskii.weatherandroid.util.Preference
 
 class ChooseLocationPresenter : ChooseLocationContract.ChooseLocationPresenter,
     BasePresenter<ChooseLocationContract.ChooseLocationView>() {
 
     override fun saveLocation(location: String) {
+        val user = Preference.user
+        user?.location = location
+        Preference.user = user
 
+        view?.onLocationSaved()
     }
 
 }
